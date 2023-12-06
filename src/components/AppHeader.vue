f4b068cf1bd4e410cc35267d1e49ba3aae28e6ac
Francesco Bonanno <francescosoapbonanno@gmail.com>
2023-12-06 16:39:22 +0100

    implementazione bandiere al posto delle nazionalità scritte

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -22,8 +22,24 @@ export default {
 
 <template>
 	<header>
+		<h1>Boolflix</h1>
 		<input v-model="store.ricerca" @keyup.enter="confirm" type="text" placeholder="Cerca Film...">
 	</header>
 </template>
 
-<style></style>
+<style>
+header {
+	background-color: black;
+	width: 100%;
+	height: 15vh;
+	display: flex;
+	justify-content: space-around;
+	align-items: center;
+}
+
+header h1 {
+	font-size: 2em;
+	font-weight: bold;
+	color: rgb(243, 8, 8);
+}
+</style>
--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -14,10 +14,43 @@ export default {
 
 <template>
 	<section id="wrapper">
-		<div v-for="card in store.contenitore" class="cardcontainer">
+		<div v-for="card in  store.contenitore " class="cardcontainer">
 			<img src="" alt="img">
+			<h2>{{ card.title }}</h2>
 			<h2>{{ card.original_title }}</h2>
-			<p></p>
+			<p v-if="card.original_language === 'en'">
+				<img src="../assets/img/gb.png" alt="">
+			</p>
+			<p v-else-if="card.original_language === 'ja'">
+				<img src="../assets/img/jp.png" alt="">
+			</p>
+			<p v-else-if="card.original_language === 'de'">
+				<img src="../assets/img/de.png" alt="">
+			</p>
+			<p v-else-if="card.original_language === 'it'">
+				<img src="../assets/img/it.png" alt="">
+			</p>
+			<p v-else-if="card.original_language === 'ko'">
+				<img src="../assets/img/kr.png" alt="">
+			</p>
+			<p v-else-if="card.original_language === 'fr'">
+				<img src="../assets/img/fr.png" alt="">
+			</p>
+			<p v-else-if="card.original_language === 'no'">
+				<img src="../assets/img/no.png" alt="">
+			</p>
+			<p v-else-if="card.original_language === 'pt'">
+				<img src="../assets/img/pt.png" alt="">
+			</p>
+			<p v-else-if="card.original_language === 'cn'">
+				<img src="../assets/img/cn.png" alt="">
+			</p>
+			<p v-else>
+				{{ card.original_language }}
+			</p>
+
+
+			<span>{{ card.vote_average }}</span>
 		</div>
 	</section>
 </template>
@@ -26,12 +59,24 @@ export default {
 #wrapper {
 	width: 100%;
 	background-color: rgb(63, 63, 63);
-	height: 100vh;
+	height: 85vh;
+	display: flex;
+	justify-content: space-around;
+	align-items: center;
+	flex-wrap: wrap;
+	overflow-y: auto;
+
 }
 
 .cardcontainer {
-	background-color: rgb(206, 41, 41);
-	width: 30%;
-	height: 30%;
+	background-color: rgb(175, 42, 42);
+	width: 15%;
+	height: 40%;
+	border: 2PX solid rgb(80, 79, 79);
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	align-items: center;
+	flex-wrap: wrap;
 }
 </style>
